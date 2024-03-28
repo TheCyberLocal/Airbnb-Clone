57b2b591ea62445440919d83b86e44c299a24d3b
Timothy <tim.3imt@gmail.com>
2024-03-28 15:37:47 -0500

    Add First and Last Name to User Signup and Validation

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -19,7 +19,14 @@ const validateSignup = [
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
     .withMessage("Please provide a username with at least 4 characters."),
-  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("firstName")
+    .isString()
+    .isLength({ min: 1, max: 20 })
+    .withMessage("First name must be a string with length between 1 and 20."),
+  check("lastName")
+    .isString()
+    .isLength({ min: 1, max: 20 })
+    .withMessage("Last name must be a string with length between 1 and 20."),
   check("password")
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
@@ -29,14 +36,22 @@ const validateSignup = [
 
 // Sign up
 router.post("/", validateSignup, async (req, res) => {
-  const { email, password, username } = req.body;
+  const { email, password, username, firstName, lastName } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
-  const user = await User.create({ email, username, hashedPassword });
+  const user = await User.create({
+    email,
+    username,
+    firstName,
+    lastName,
+    hashedPassword,
+  });
 
   const safeUser = {
     id: user.id,
     email: user.email,
     username: user.username,
+    firstName: user.firstName,
+    lastName: user.lastName,
   };
 
   await setTokenCookie(res, safeUser);

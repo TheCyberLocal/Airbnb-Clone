4c9aa64e4a6a214205b7c08ee4ed75f8beac53a8
Timothy <tim.3imt@gmail.com>
2024-04-04 16:00:53 -0500

    Add Image to Review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -51,4 +51,63 @@ router.get("/current", requireAuth, async (req, res, next) => {
   });
 });
 
+// Add image to review
+router.post("/:reviewId/images", requireAuth, async (req, res) => {
+  const reviewId = parseInt(req.params.reviewId);
+  if (isNaN(reviewId) || reviewId < 1) {
+    const err = new Error("Review couldn't be found");
+    err.status = 404;
+    throw err;
+  }
+
+  // Review must exist
+  const reviewExists = await Review.findByPk(reviewId);
+  if (!reviewExists) {
+    const err = new Error("Review couldn't be found");
+    err.status = 404;
+    throw err;
+  }
+
+  // Review must belong to User
+  if (reviewExists.userId !== req.user.id) {
+    const err = new Error("Forbidden");
+    err.status = 403;
+    throw err;
+  }
+
+  // Spot can't exceed 10 images
+  const reviewImages = await ReviewImage.findAll({
+    where: {
+      reviewId,
+    },
+  });
+  if (reviewImages.length > 10) {
+    const err = new Error(
+      "Maximum number of images for this resource was reached"
+    );
+    err.status = 403;
+    throw err;
+  }
+
+  // URL must exist
+  const { url } = req.body;
+  if (typeof url !== "string" || !url.length) {
+    const err = new Error("Review URL is required");
+    err.status = 404;
+    throw err;
+  }
+
+  const newImage = await ReviewImage.create({
+    reviewId,
+    url,
+  });
+
+  // Clean response
+  newImage.dataValues.reviewId = undefined;
+  newImage.dataValues.updatedAt = undefined;
+  newImage.dataValues.createdAt = undefined;
+
+  res.json(newImage);
+});
+
 module.exports = router;

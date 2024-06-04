d581613fb875e2f6ea1b26eee0f0fb2e4c5acd96
Timothy Macfarlane <tim.3imt@gmail.com>
2024-06-03 19:13:38 -0500

    Spot Page Reserve Button and Description

--- frontend/src/components/SpotPage/SpotPage.jsx
+++ frontend/src/components/SpotPage/SpotPage.jsx
@@ -3,18 +3,29 @@ import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSpot } from "../../store/spots";
+import { FaStar } from "react-icons/fa";
 
 function SpotPage() {
   const { spotId } = useParams();
   const dispatch = useDispatch();
   const spot = useSelector((state) => state.spots[spotId]);
+  console.log("spot", spot);
 
   useEffect(() => {
     dispatch(fetchSpot(spotId));
   }, [dispatch, spotId]);
 
-  const { name, city, state, country, description, numReviews, SpotImages } =
-    spot;
+  const {
+    name,
+    city,
+    state,
+    country,
+    description,
+    avgStarRating,
+    price,
+    numReviews,
+    SpotImages,
+  } = spot;
 
   const images = Object.values(SpotImages || {});
   const previewImage =
@@ -44,11 +55,31 @@ function SpotPage() {
         </div>
       </div>
       <div id="details">
-        <span>
-          <div id="details-title">{`Hosted by Firstname Lastname`}</div>
+        <div id="hostedBy">
+          <div id="title">{`Hosted by Firstname Lastname`}</div>
           <div id="description">{`${description}`}</div>
-        </span>
-        <span id="reserve">Reservations</span>
+        </div>
+        <div id="reserve">
+          <div id="reserve-card">
+            <div id="reserve-card-head">
+              <div id="reserve-card-price">${price}</div>
+              <div id="reserve-card-text">
+                <span id="reserve-card-text1">night</span>
+                <span id="reserve-card-text2">
+                  <FaStar /> {avgStarRating?.toFixed(1) || "N/A"} - {numReviews}{" "}
+                  reviews
+                </span>
+              </div>
+            </div>
+            <button
+              onClick={() => alert("Feature Coming Soon...")}
+              className="clickable"
+              id="reserve-card-button"
+            >
+              Reserve
+            </button>
+          </div>
+        </div>
       </div>
       <div id="reviews">All Reviews Below - count {numReviews}</div>
     </div>
